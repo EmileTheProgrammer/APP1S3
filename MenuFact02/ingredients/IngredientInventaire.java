13c0a721ceb133ef4660349a6e9be4ded8c268d0
samuel <samueltrep@hotmail.com>
2022-05-06 09:11:37 -0400

    MenuFact02

---
+++ MenuFact02/ingredients/IngredientInventaire.java
@@ -0,0 +1,25 @@
+package ingredients;
+
+import ingredients.exceptions.IngredientException;
+
+public class IngredientInventaire {
+    private Ingredient ingredient;
+    private int quantite;
+
+    public IngredientInventaire(Ingredient ingredient, int quantite) {
+        this.ingredient = ingredient;
+        this.quantite = quantite;
+    }
+
+    public int getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(int quantite) throws IngredientException{
+
+        if (quantite < 0)
+            throw new IngredientException("Il n'est pas possible d'avoir une quantité negative");
+        else
+            this.quantite = quantite;
+    }
+}
---
+++ MenuFact02/menufact/Client.java
@@ -0,0 +1,57 @@
+package menufact;
+
+public class Client {
+    private int idClient;
+    private String nom;
+    private String numeroCarteCredit;
+    private static Client client;
+
+     private Client(int idClient, String nom, String numeroCarteCredit) {
+        this.idClient = idClient;
+        this.nom = nom;
+        this.numeroCarteCredit = numeroCarteCredit;
+    }
+
+    public static Client getInstance(int idClient, String nom, String numeroCarteCredit){
+        if(client == null)
+            client = new Client(idClient, nom, numeroCarteCredit);
+        return client;
+    }
+
+    public int getIdClient() {
+        return idClient;
+    }
+
+    public void setIdClient(int idClient) {
+        this.idClient = idClient;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNumeroCarteCredit() {
+        return numeroCarteCredit;
+    }
+
+    public void setNumeroCarteCredit(String numeroCarteCredit) {
+        this.numeroCarteCredit = numeroCarteCredit;
+    }
+
+    @Override
+    public String toString() {
+        return "menufact.Client{" +
+                "idClient=" + idClient +
+                ", nom='" + nom + '\'' +
+                ", numeroCarteCredit='" + numeroCarteCredit + '\'' +
+                '}';
+    }
+}
+/*
+@startuml
+class menufact.Client{}
+* */
